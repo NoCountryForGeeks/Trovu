7beb7e7b07398a1b917337d30db36e713428c281
Sergio Gallardo Sales <maktub82@macbook.local>
2018-09-08 20:13:35 +0200

    Add basic Componets

--- app/modules/App.js
+++ app/modules/App.js
@@ -1,39 +1,16 @@
 import React, { Component, Fragment } from 'react';
 
-import LanguageSelector from 'shared/i18n/LanguageSelector';
-import { Translate } from 'shared/i18n/Translate';
-
-import noCountryForGeeksLogo from 'assets/images/logo.svg';
-import { mainTitle, logo, selector } from './app.scss';
+import { Header } from './App/Header';
+import { Footer } from './App/Footer';
+import { MasterPage } from './App/MasterPage';
 
 export class App extends Component {
 	render() {
 		return (
 			<Fragment>
-				<header>
-					<h1 className={mainTitle}>
-						<Translate i18nKey={'baseProject'} />
-					</h1>
-				</header>
-				<main>
-					<img
-						src={noCountryForGeeksLogo}
-						alt='No country for geeks'
-						className={logo}
-					/>
-					<h3>
-						<Translate i18nKey={'presentation'} />
-					</h3>
-					<span>
-						<Translate i18nKey={'description'} />
-					</span>
-					<div className={selector}>
-						<label>
-							<Translate i18nKey={'language'} />
-						</label>{' '}
-						<LanguageSelector />
-					</div>
-				</main>
+				<Header />
+				<MasterPage />
+				<Footer />
 			</Fragment>
 		);
 	}
